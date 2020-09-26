0ac0e33ae1c3862673eb3f7dd88d35acbcd5ecc6
Jean-Baptiste THERY <jnb.thery@gmail.com>
2020-09-26 23:49:11 +0200

    add firebase to project
    Signed-off-by: Jean-Baptiste THERY <jnb.thery@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +10,13 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.routeName,
+      routes: {
+        WelcomeScreen.routeName: (context) => WelcomeScreen(),
+        LoginScreen.routeName: (context) => LoginScreen(),
+        RegistrationScreen.routeName: (context) => RegistrationScreen(),
+        ChatScreen.routeName: (context) => ChatScreen(),
+      },
     );
   }
 }
